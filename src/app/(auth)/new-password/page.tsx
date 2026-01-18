5ed6157a239e2ba20dd955101ee1c94d929f5025
shiv-codes-design <shiv.codes.design@gmail.com>
2026-01-18 23:12:09 +0530

    auth module design complete

---
+++ src/app/(auth)/new-password/page.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NewPasswordPage = () => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center max-w-[465px] mx-auto">
+      <h1 className="text-center font-bold text-[28px] 1xl:text-[34px] text-white mb-1">
+        Set New Password
+      </h1>
+      <p className="text-center font-medium text-sm 1xl:text-base text-white mb-5">
+        Don’t worry! Enter your registered email address to get password reset
+        instructions.
+      </p>
+      <form
+        action=""
+        className="w-full rounded-[25px] 1xl:rounded-[35px] border border-solid p-5 1xl:p-[30px] bg-white border-theme-white-100 shadow-sm"
+      >
+        <div className="w-full mb-4">
+          <label
+            htmlFor=""
+            className="text-base 1xl:text-lg font-semibold text-theme-black-50 inline-flex mb-[10px]"
+          >
+           New Password
+          </label>
+          <div className="w-full flex items-center relative">
+            <input
+              type="text"
+              placeholder="Enter Password"
+              className="pr-14
+              text-base 1xl:text-lg font-medium placeholder:text-theme-black-150 w-full min-h-[56px] 1xl:min-h-[66px] 1xl:px-5 px-4 py-3 1xl:py-4 rounded-xl 1xl:rounded-[15px] border border-theme-white-100 bg-theme-white-150 outline-none text-theme-darkblue-00
+              "
+            />
+            <button className="size-4 1xl:size-5 flex items-center justify-center cursor-pointer hover:opacity-75 absolute right-4 1xl:right-5">
+              <Image
+                className="size-full"
+                src={"assets/images/svg/eye-close.svg"}
+                width={18}
+                height={18}
+                alt="icon"
+              ></Image>
+            </button>
+          </div>
+        </div>
+        <div className="w-full mb-4">
+          <label
+            htmlFor=""
+            className="text-base 1xl:text-lg font-semibold text-theme-black-50 inline-flex mb-[10px]"
+          >
+           Confirm New Password
+          </label>
+          <div className="w-full flex items-center relative">
+            <input
+              type="text"
+              placeholder="Enter Password"
+              className="pr-14
+              text-base 1xl:text-lg font-medium placeholder:text-theme-black-150 w-full min-h-[56px] 1xl:min-h-[66px] 1xl:px-5 px-4 py-3 1xl:py-4 rounded-xl 1xl:rounded-[15px] border border-theme-white-100 bg-theme-white-150 outline-none text-theme-darkblue-00
+              "
+            />
+            <button className="size-4 1xl:size-5 flex items-center justify-center cursor-pointer hover:opacity-75 absolute right-4 1xl:right-5">
+              <Image
+                className="size-full"
+                src={"assets/images/svg/eye-close.svg"}
+                width={18}
+                height={18}
+                alt="icon"
+              ></Image>
+            </button>
+          </div>
+        </div>
+        <div className="w-full mt-[30px]">
+          <button
+            type="submit"
+            title="Login"
+            className="w-full p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
+          >
+            <span className="relative top-0.5">Submit</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewPasswordPage;
--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -5,43 +5,54 @@ import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 
 const WelcomePage = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modal1IsOpen, set1IsOpen] = useState(false);
+  const [modal2IsOpen, set2IsOpen] = useState(false);
+  const [modal3IsOpen, set3IsOpen] = useState(false);
 
   useEffect(() => {
     // Required for accessibility & to avoid SSR warning
     Modal.setAppElement("body");
   }, []);
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
+  const open1Modal = () => set1IsOpen(true);
+  const open2Modal = () => set2IsOpen(true);
+  const open3Modal = () => set3IsOpen(true);
+  const close1Modal = () => set1IsOpen(false);
+  const close2Modal = () => set2IsOpen(false);
+  const close3Modal = () => set3IsOpen(false);
 
   return (
     <>
       <div className="w-full flex flex-col items-center justify-center max-w-[695px] mx-auto">
         <div className="w-full grid grid-cols-3 gap-4">
           <button
-            onClick={openModal}
+            onClick={open1Modal}
             className="p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
           >
             <span className="relative top-0.5">Modal 1</span>
           </button>
 
-          <button className="p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]">
+          <button
+            onClick={open2Modal}
+            className="p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
+          >
             <span className="relative top-0.5">Modal 2</span>
           </button>
 
-          <button className="p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]">
+          <button
+            onClick={open3Modal}
+            className="p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
+          >
             <span className="relative top-0.5">Modal 3</span>
           </button>
         </div>
       </div>
-
       <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
+        isOpen={modal1IsOpen}
+        onRequestClose={close1Modal}
         contentLabel="Model 1"
         overlayClassName="fixed inset-0 bg-black/50 flex items-center justify-center"
-        className="relative w-full max-w-[488px] rounded-[25px] 1xl:rounded-[35px] border border-solid p-[30px] bg-white border-theme-white-100 shadow-sm"
+        className="relative w-full max-w-[500px] rounded-[25px] 1xl:rounded-[35px] border border-solid p-[30px] bg-white border-theme-white-100 shadow-sm"
       >
         <Image
           className="w-[66px] h-auto mb-[30px] mx-auto"
@@ -63,6 +74,61 @@ const WelcomePage = () => {
         <p className="text-center font-medium text-sm 1xl:text-base text-theme-black-50">
           Please Wait until the admin approves your Profile.
         </p>
+      </Modal>
+      <Modal
+        isOpen={modal2IsOpen}
+        onRequestClose={close2Modal}
+        contentLabel="Model 2"
+        overlayClassName="fixed inset-0 bg-black/50 flex items-center justify-center"
+        className="relative w-full max-w-[500px] rounded-[25px] 1xl:rounded-[35px] border border-solid p-[30px] bg-white border-theme-white-100 shadow-sm"
+      >
+        <Image
+          className="w-[311px] h-auto mb-[30px] mx-auto"
+          src={"/assets/images/png/image-4.png"}
+          width={311}
+          height={221}
+          alt="img"
+        ></Image>
+        <h1 className="text-center font-bold text-[28px] 1xl:text-[34px] text-theme-black-00 mb-1">
+          Your account has been Created!
+        </h1>{" "}
+        <p className="text-center font-medium text-sm 1xl:text-base text-theme-black-50 mb-[30px]">
+          Are you ready to manage your own wealth?
+        </p>
+        <button
+          title="Continue Dashboard"
+          className="w-full p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
+        >
+          <span>Continue Dashboard</span>
+        </button>
+      </Modal>
+      <Modal
+        isOpen={modal3IsOpen}
+        onRequestClose={close3Modal}
+        contentLabel="Model 3"
+        overlayClassName="fixed inset-0 bg-black/50 flex items-center justify-center"
+        className="relative w-full max-w-[500px] rounded-[25px] 1xl:rounded-[35px] border border-solid p-[30px] bg-white border-theme-white-100 shadow-sm"
+      >
+        <Image
+          className="w-[311px] h-auto mb-[30px] mx-auto"
+          src={"/assets/images/png/image-5.png"}
+          width={311}
+          height={221}
+          alt="img"
+        ></Image>
+        <h1 className="text-center font-bold text-[28px] 1xl:text-[34px] text-theme-black-00 mb-1">
+          Opps!
+        </h1>{" "}
+        <p className="text-center font-medium text-sm 1xl:text-base text-theme-black-50 mb-[30px]">
+          Your documents have been rejected. Please upload them again correctly
+          and complete your KYC.
+        </p>
+        <button
+          title="Continue Dashboard"
+          className="w-full p-2 h-12 1xl:h-14 cursor-pointer flex items-center justify-center gap-2.5 text-white text-base 1xl:text-lg font-semibold bg-theme-purple-50 hover:bg-theme-purple-00 rounded-[10px]"
+        >
+          <span>Sorry, Continue Re KYC</span>
+        </button>
       </Modal>
     </>
   );
